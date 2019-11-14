27c506a7d7a21789f7209e7b708c01572a06fd5e
Nathan Souffan <souffan.nathan@gmail.com>
2019-11-14 17:04:34 +0100

    first commit

---
+++ src/main/java/fr/univparis/azul/App.java
@@ -0,0 +1,13 @@
+package fr.univparis.azul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
