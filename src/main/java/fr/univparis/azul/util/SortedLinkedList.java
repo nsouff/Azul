243d7f99f6940622b83992cfaf62ebdcb41048f1
Romain Bouarah <Rom.Bou@hotmail.fr>
2019-12-04 23:02:19 +0100

    Add SortedLinkedList class

---
+++ src/main/java/fr/univparis/azul/util/SortedLinkedList.java
@@ -0,0 +1,29 @@
+package fr.univparis.azul.util;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class SortedLinkedList<E> {
+    LinkedList<E> list;
+    Comparator<E> comp;
+
+    public SortedLinkedList(Comparator<E> c) {
+	list = new LinkedList<E>();
+	comp = c;
+    }
+
+    public void add(E e) {
+	int i = 0;
+	
+	while( i < list.size() && comp.compare(list.get(i),e) < 0 ) {
+	    i++;
+	}
+	
+	list.add(i,e);
+    }
+
+    public E get(int index) {
+	return list.get(index);
+    }
+
+}
---
+++ src/test/java/fr/univparis/azul/util/SortedLinkedListTest.java
@@ -0,0 +1,36 @@
+package fr.univparis.azul.util;
+
+import static org.junit.Assert.*;
+
+import fr.univparis.azul.tile.ColoredTile;
+import fr.univparis.azul.util.SortedLinkedList;
+
+import java.util.*;
+import org.junit.Test;
+
+public class SortedLinkedListTest {
+
+    @Test
+    public void addTest() {
+	SortedLinkedList<ColoredTile> list = new SortedLinkedList( new Comparator<ColoredTile>() {
+		public int compare(ColoredTile t1, ColoredTile t2) {
+		    return t1.getColor().ordinal() - t2.getColor().ordinal(); // BLUE < RED < GREEN < BLACK < YELLOW
+		}
+	    });
+
+	ColoredTile t1 = new ColoredTile(ColoredTile.Colors.BLUE);
+	ColoredTile t2 = new ColoredTile(ColoredTile.Colors.BLUE);
+	ColoredTile t3 = new ColoredTile(ColoredTile.Colors.RED);
+	ColoredTile t4 = new ColoredTile(ColoredTile.Colors.GREEN);
+
+	list.add(t1);
+	list.add(t2);
+	list.add(t3);
+	list.add(t4);
+
+	assertSame(list.get(0), t2);
+	assertSame(list.get(1), t1);
+	assertSame(list.get(2), t3);
+	assertSame(list.get(3), t4);
+    }
+}
