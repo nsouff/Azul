4d796eaae4e520dc85b8a5290b3288deed4958da
Nathan Souffan <souffan.nathan@gmail.com>
2019-12-07 15:43:12 +0100

    Add implementation of Wall

--- src/main/java/fr/univparis/azul/area/Wall.java
+++ src/main/java/fr/univparis/azul/area/Wall.java
@@ -6,17 +6,34 @@ import java.util.ArrayList;
 
 public class Wall extends PlayerArea implements IndexedArea {
 
-  private ArrayList<ArrayList<Tile>> tiles;
+  private ArrayList<ArrayList<ColoredTile>> tiles;
+
+  public Wall() {
+    tiles = new ArrayList<ArrayList<ColoredTile>>(5);
+    for (int i = 0; i < 5; i++) {tiles.add(new ArrayList<ColoredTile>(5));}
+  }
 
   @Override
-  public void add(int index, Tile tile){}
+  public void add(int index, Tile tile){
+    if (! (tile instanceof ColoredTile)) throw new IllegalArgumentException("Wall only accept ColoredTile");
+    tiles.get(index).add((ColoredTile) tile);
+  }
 
 
   @Override
-  public int size() {return -1;}
+  public int size() {
+    int size = 0;
+    for (ArrayList<ColoredTile> list : tiles) {
+      size += list.size();
+    }
+    return size;
+  }
 
   @Override
-  public boolean isEmpty() {return true;}
+  public boolean isEmpty() {
+    for (int i = 0; i < 5; i++) if (!tiles.get(i).isEmpty()) return false;
+    return true;
+  }
 
 
 
