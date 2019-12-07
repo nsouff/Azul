a21b6e207767c1a4c351097e3ec967b3c730ba0f
Nathan Souffan <souffan.nathan@gmail.com>
2019-12-07 17:50:37 +0100

    Add implementation of Factory with fill method

--- src/main/java/fr/univparis/azul/area/Bag.java
+++ src/main/java/fr/univparis/azul/area/Bag.java
@@ -6,10 +6,17 @@ import fr.univparis.azul.tile.*;
 
 public class Bag extends OffSideArea {
 
+  private class BagEmptyException extends IllegalStateException{}
+
   public void refill(Trash trash) {
     tiles = trash.getTiles();
     trash.tiles = new LinkedList<Tile>();
     Collections.shuffle(tiles);
+  }
+
+  public Tile poll() {
+    if (isEmpty()) throw new BagEmptyException();
+    return tiles.poll();
   }
 
 }
--- src/main/java/fr/univparis/azul/area/Factory.java
+++ src/main/java/fr/univparis/azul/area/Factory.java
@@ -10,13 +10,27 @@ public class Factory extends CommonArea {
     {
       tiles = new ArrayList<Tile>(size);
     }
-    @Override
-    public int size() {return -1;}
+
+    private class FullFactoryException extends IllegalStateException {}
+
+    private class NotEmptyFactoryException extends IllegalStateException {}
 
     @Override
-    public boolean isEmpty(){return true;}
+    public int size() {return tiles.size();}
 
     @Override
-    public void add(Tile tile) {}
+    public boolean isEmpty(){return tiles.isEmpty();}
+
+    @Override
+    public void add(Tile tile) {
+      if (! (tile instanceof ColoredTile)) throw new IllegalArgumentException("tile must be a ColoredTile in the Factory");
+      if (size() >= size) throw new FullFactoryException();
+      tiles.add((ColoredTile) tile);
+    }
+
+    public void fill(Bag bag) {
+      if (!isEmpty()) throw new NotEmptyFactoryException();
+      for (int i = 0; i < size; i++) add(bag.poll());
+    }
 
 }
