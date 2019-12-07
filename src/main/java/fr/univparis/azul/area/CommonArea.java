5f79553e5b46ae6bec0ef456f40cc57840305501
Nathan Souffan <souffan.nathan@gmail.com>
2019-12-07 19:08:44 +0100

    Add getSameColorTile method for CommonArea

--- src/main/java/fr/univparis/azul/area/CommonArea.java
+++ src/main/java/fr/univparis/azul/area/CommonArea.java
@@ -1,9 +1,24 @@
 package fr.univparis.azul.area;
 
-import java.util.List;
+import java.util.*;
 
-import fr.univparis.azul.tile.Tile;
+import fr.univparis.azul.tile.*;
 
 public abstract class CommonArea implements UnindexedArea {
   protected List<Tile> tiles;
+
+  public List<ColoredTile> getSameColorTile(ColoredTile.Colors c) {
+    ArrayList<ColoredTile> list = new ArrayList<ColoredTile>();
+    Iterator<Tile> it = tiles.iterator();
+    while (it.hasNext()) {
+      Tile t = it.next();
+
+      if (t instanceof ColoredTile && ( ((ColoredTile) t).getColor() == c )) {
+        it.remove();
+        list.add((ColoredTile)t);
+      }
+    }
+
+    return list;
+  }
 }
