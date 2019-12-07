0742a6cc2c31ad71120cf8073070b666b0a0cd13
Nathan Souffan <souffan.nathan@gmail.com>
2019-12-07 15:50:15 +0100

    Add implementation of Floor

--- src/main/java/fr/univparis/azul/area/Floor.java
+++ src/main/java/fr/univparis/azul/area/Floor.java
@@ -9,13 +9,13 @@ public class Floor extends PlayerArea implements UnindexedArea {
   private ArrayList<Tile> tiles;
 
   @Override
-  public int size() {return -1;}
+  public int size() {return tiles.size();}
 
   @Override
-  public boolean isEmpty(){return true;}
+  public boolean isEmpty(){return tiles.isEmpty();}
 
   @Override
-  public void add(Tile tile) {}
+  public void add(Tile tile) {tiles.add(tile);}
 
 
 }
