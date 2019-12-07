a91842e8248d05f724ae3b1f290c92a9043a40ad
Nathan Souffan <souffan.nathan@gmail.com>
2019-12-07 18:25:27 +0100

    Add Test for Factory

---
+++ src/test/java/fr/univparis/azul/area/FactoryTest.java
@@ -0,0 +1,48 @@
+package fr.univparis.azul.area;
+import fr.univparis.azul.tile.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+public class FactoryTest {
+
+  @Test
+  public void addTest() {
+    ColoredTile t = new ColoredTile(ColoredTile.Colors.BLUE);
+    Factory f = new Factory();
+    f.add(t);
+    assertEquals(t, f.tiles.get(0));
+  }
+
+  @Test
+  public void fillTest() {
+    Bag bag  = new Bag();
+    ColoredTile t1 = new ColoredTile(ColoredTile.Colors.GREEN);
+    ColoredTile t2 = new ColoredTile(ColoredTile.Colors.GREEN);
+    ColoredTile t3 = new ColoredTile(ColoredTile.Colors.GREEN);
+    ColoredTile t4 = new ColoredTile(ColoredTile.Colors.GREEN);
+    bag.add(t1); bag.add(t2); bag.add(t3); bag.add(t4);
+    Factory f = new Factory();
+    f.fill(bag);
+    assertEquals(t1, f.tiles.get(0));
+    assertEquals(t2, f.tiles.get(1));
+    assertEquals(t3, f.tiles.get(2));
+    assertEquals(t4, f.tiles.get(3));
+  }
+
+  @Test(expected = Factory.NotEmptyFactoryException.class)
+  public void notEmptyExceptionTest() {
+    Factory f = new Factory();
+    f.add(new ColoredTile(ColoredTile.Colors.BLUE));
+    f.fill(new Bag());
+  }
+
+  @Test(expected = Factory.FullFactoryException.class)
+  public void FullFactoryExceptionTest() {
+    Factory f = new Factory();
+    for (int i = 0; i < 5; i++) f.add(new ColoredTile(ColoredTile.Colors.BLUE));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void notColoredTileExceptionTest() {
+    new Factory().add(new FirstTile());
+  }
+}
