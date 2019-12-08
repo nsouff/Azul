a4bab07b476b350c3f156e1e967364fd5409032e
Nathan Souffan <souffan.nathan@gmail.com>
2019-12-08 16:05:53 +0100

    Change on SortedLinkedList, it now implements List<E>

--- src/main/java/fr/univparis/azul/util/SortedLinkedList.java
+++ src/main/java/fr/univparis/azul/util/SortedLinkedList.java
@@ -4,52 +4,87 @@ import java.lang.Iterable;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Collection;
+import java.util.ListIterator;
 
-public class SortedLinkedList<E> implements Iterable<E> {
-    private LinkedList<E> list;
-    private Comparator<E> comp;
+public class SortedLinkedList<E> implements List<E> {
+  private LinkedList<E> list;
+  private Comparator<E> comp;
 
-    public SortedLinkedList(Comparator<E> c) {
-	list = new LinkedList<E>();
-	comp = c;
+  public SortedLinkedList(Comparator<E> c) {
+    list = new LinkedList<E>();
+    comp = c;
+  }
+
+  public boolean add(E e) {
+    int i = 0;
+
+    while( i < list.size() && comp.compare(list.get(i),e) < 0 ) {
+      i++;
     }
 
-    public void add(E e) {
-	int i = 0;
-	
-	while( i < list.size() && comp.compare(list.get(i),e) < 0 ) {
-	    i++;
-	}
-	
-	list.add(i,e);
-    }
+    list.add(i,e);
+    return true;
+  }
 
-    public E get(int index) {
-	return list.get(index);
-    }
+  public E get(int index) {
+    return list.get(index);
+  }
 
-    public boolean isEmpty() {
-	return list.isEmpty();
-    }
+  public boolean isEmpty() {
+    return list.isEmpty();
+  }
 
-    public int size() {
-	return list.size();
-    }
+  public int size() {
+    return list.size();
+  }
 
-    public E remove(int index) {
-	return list.remove(index);
-    }
+  public E remove(int index) {
+    return list.remove(index);
+  }
 
-    public boolean remove(Object o) {
-	return list.remove(o);
-    }
+  public boolean remove(Object o) {
+    return list.remove(o);
+  }
 
-    public boolean contains(Object o) {
-	return list.contains(o);
-    }
+  public boolean contains(Object o) {
+    return list.contains(o);
+  }
 
-    @Override
-    public Iterator<E> iterator() {
-	return list.iterator();
-    }
+  @Override
+  public Iterator<E> iterator() {
+    return list.iterator();
+  }
+
+  public E set(int index, E e) {throw new UnsupportedOperationException();}
+
+  public void clear() {list.clear();}
+
+  public void add(int index, E e) {throw new UnsupportedOperationException();}
+
+  public boolean addAll(int index, Collection<? extends E> c) {throw new UnsupportedOperationException();}
+
+  public boolean addAll(Collection<? extends E> c) {throw new UnsupportedOperationException();}
+
+  public boolean containsAll(Collection<?> c) {throw new UnsupportedOperationException();}
+
+  public int indexOf(Object o) {throw new UnsupportedOperationException();}
+
+  public int lastIndexOf(Object o) {throw new UnsupportedOperationException();}
+
+  public ListIterator<E> listIterator() {throw new UnsupportedOperationException();}
+
+  public ListIterator<E> listIterator(int index) {throw new UnsupportedOperationException();}
+
+  public boolean removeAll(Collection<?> c) {throw new UnsupportedOperationException();}
+
+  public boolean retainAll(Collection<?> c) {throw new UnsupportedOperationException();}
+
+  public List<E> subList(int fromIndex, int toIndex) {throw new UnsupportedOperationException();}
+
+  public Object[] toArray() {throw new UnsupportedOperationException();}
+
+  public <T> T[] toArray(T[] a) {throw new UnsupportedOperationException();}
+
 }
