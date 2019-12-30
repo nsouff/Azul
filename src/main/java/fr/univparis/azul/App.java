460cc17b31c5145fc1f3acf3dd14e6789326488d
Romain Bouarah <Rom.Bou@hotmail.fr>
2019-12-30 20:06:05 +0100

    Make Game class more sequencible
    In order to use the Game class in the game loop, the class need to be divided in more parts that tick once.

--- src/main/java/fr/univparis/azul/App.java
+++ src/main/java/fr/univparis/azul/App.java
@@ -8,7 +8,7 @@ public class App {
     public static void main( String[] args ) {
 	try {
 	    File config = new File(args[0]); // for the moment we are using the path passed in arguments
-	    GameController game = new GameController( config );
+	    
 	} catch (Exception e) {
 	    e.printStackTrace();
 	}
